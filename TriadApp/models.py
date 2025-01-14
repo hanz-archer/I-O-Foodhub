52b9fd8032c57fe27f6f6109f0d2841e8687539b
josh <joshdanielsaraa@gmail.com>
2025-01-14 15:53:39 +0800

    superadmin fix

--- TriadApp/models.py
+++ TriadApp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 import uuid
+from django.core.exceptions import ValidationError
 
 class Stall(models.Model):
     store_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
@@ -24,8 +25,19 @@ class AdminProfile(models.Model):
     username = models.CharField(max_length=50, unique=True)
     password = models.CharField(max_length=100)  # Remember to hash passwords
     contact_number = models.CharField(max_length=15)
-    stall = models.ForeignKey('Stall', on_delete=models.CASCADE, related_name="admins")
+    stall = models.ForeignKey('Stall', on_delete=models.CASCADE, related_name="admins", to_field='store_id')
+
+    def clean(self):
+        # Check if this stall already has an admin
+        if self.stall:
+            existing_admin = AdminProfile.objects.filter(stall=self.stall).exclude(id=self.id).exists()
+            if existing_admin:
+                raise ValidationError(f'Stall {self.stall.name} already has an admin assigned.')
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.firstname} {self.lastname} - {self.username}"
+        return f"{self.firstname} {self.lastname} - {self.stall.store_id}"
 
